9977652be98b2fc42de57b4d44b781261f05ab9c
annadehtiarova <60813021+annadehtiarova@users.noreply.github.com>
2024-12-17 13:07:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -149,3 +149,15 @@ const renderSongs = (array) => {
     playButton.classList.remove("playing");
     audio.pause();
   };
+
+  const playNextSong = () => {
+
+    if (userData?.currentSong === null) {
+      playSong(userData?.songs[0].id);
+    } else {
+      const currentSongIndex = getCurrentSongIndex();
+      const nextSong = userData?.songs[currentSongIndex + 1];
+  
+      playSong(nextSong.id);
+    }
+  };
